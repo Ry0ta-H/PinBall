85d5c4b447e04f25a036667adab27143a2f794ab
原　諒太 <ryopityu@yahoo.co.jp>
2019-05-24 21:08:22 +0900

    Lesson5の課題提出
    課題：得点を表示しようを提出
    発展課題はなし

---
+++ Assets/Score.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    //スコアを表示する
+    public GameObject scoreText;
+
+    //スコアを表するテキスト
+    private int score = 0;
+
+    // Use this for initialization
+
+    void Start() {
+        this.scoreText = GameObject.Find("scoreText");
+        score = 0;
+
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        string tag = other.gameObject.tag;
+        if (tag == "LargeCloudTag")
+        {
+            score += 100;
+        }
+        else if (tag == "SmallCloudTag" || tag == "LargeStarTag")
+        {
+            score += 50;
+        }
+        else if (tag == "SmallStarTag" )
+        {
+            score += 1;
+        }
+        Debug.Log(score);
+        this.scoreText.GetComponent<Text>().text = string.Format("Score:{0}", score);
+    }
+}
